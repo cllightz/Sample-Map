33ece56aeaeab8899accfd834b371b5661b599ef
狼電 <hazardkirby@gmail.com>
2015-10-19 03:18:45 +0900

    0.1.0 復元
    API キー登録
    Polyline 表示
    Directions API 用 URL 生成

--- SampleMap/app/src/main/java/com/example/foobar/samplemap/MapsActivity.java
+++ SampleMap/app/src/main/java/com/example/foobar/samplemap/MapsActivity.java
@@ -1,14 +1,20 @@
 package com.example.foobar.samplemap;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 	
@@ -37,10 +43,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 	@Override
 	public void onMapReady( GoogleMap googleMap ) {
 		mMap = googleMap;
-		
+
+		/* Marker
 		// Add a marker in Sydney and move the camera
-		LatLng sydney = new LatLng( -34, 151 );
-		mMap.addMarker( new MarkerOptions().position( sydney ).title( "Marker in Sydney" ) );
-		mMap.moveCamera( CameraUpdateFactory.newLatLng( sydney ) );
+		LatLng sydney = new LatLng(-34, 151);
+		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+		*/
+
+		// Add Polylines
+		LatLng sta = new LatLng( 35.606418, 139.749556 );
+		LatLng end = new LatLng( 35.604742, 139.748413 );
+		PolylineOptions options = new PolylineOptions().add( sta, end ).color( Color.RED );
+		mMap.addPolyline( options );
+
+		// Move Camera
+		float zoom = 17.0f;
+		float tilt = 0.0f;
+		float bear = 0.0f;
+		CameraPosition position = new CameraPosition( sta, zoom, tilt, bear );
+		CameraUpdate update = CameraUpdateFactory.newCameraPosition( position );
+		mMap.moveCamera( update );
+
+		// Get a Direction
+		String key = getResources().getString( R.string.server_key );
+		Uri.Builder builder = new Uri.Builder();
+		builder.scheme( "https" );
+		builder.authority( "maps.googleapis.com" );
+		builder.path( "/maps/api/directions/json" );
+		builder.appendQueryParameter( "origin", "35.608787,139.749507" );
+		builder.appendQueryParameter( "destination", "35.606125,139.749354" );
+		builder.appendQueryParameter( "mode", "walking" );
+		builder.appendQueryParameter( "avoid", "indoor" );
+		builder.appendQueryParameter( "units", "metric" );
+		builder.appendQueryParameter( "key", key );
+
+		Log.d( "uri", builder.build().toString() );
+		// Toast.makeText( MapsActivity.this, builder.build().toString(), Toast.LENGTH_SHORT ).show();
 	}
 }
